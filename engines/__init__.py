bf87005c0a9acc6a40ccf65db18df175c988cdd5
samitha-esc <samithab686@gmail.com>
2026-06-04 18:26:12 +0530

    more

--- engines/__init__.py
+++ engines/__init__.py
@@ -2,6 +2,6 @@
 from .base_engine import BaseEngine
 from .engine_color import ColorEngine
 from .engine_contours import ContoursEngine
-from .engine_medipipe import MediPipeEngine
+# from .engine_medipipe import MediPipeEngine  # COMMENTED OUT - not using yet
 
-__all__ = ["BaseEngine", "ColorEngine", "ContoursEngine", "MediPipeEngine"]
+__all__ = ["BaseEngine", "ColorEngine", "ContoursEngine"]  # Removed MediPipeEngine
